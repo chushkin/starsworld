ff4da2174e12da9995ea4890059a9e28b94a0753
Eugene Chushkin <eugene.chushkin@gmail.com>
2016-05-30 23:25:29 +0300

    added project and .gitignore

---
+++ Assets/Scripts/CameraSettings.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public Camera camera;
+
+	private float lastHeiht = 0;
+
+	void OnEnable()
+	{
+		if (!camera)
+		{
+			Debug.Log ("Camera is not set");
+			enabled = false;			
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (lastHeight != Screen.height)
+		{
+			lastHeight = Screen.height;
+			camera.orthographicSize = lastHeight / 2;
+		}
+	}
+}
